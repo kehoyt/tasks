2a08c1709af8770b3b6a6cba1e6f31bd08c7d290
Katie <kehoyt@udel.edu>
2022-02-12 17:15:38 -0500

    task 3 wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -9,9 +10,41 @@ function App(): JSX.Element {
             </header>
             <p>Hello World!</p>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                Edit <code>src/App.tsx</code> and save! This page will
                 automatically reload.
             </p>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="col1-header">
+                                Task 3: HTML and CSS
+                            </h1>
+                            <ol>
+                                <li>Add a header</li>
+                                <li>Add an image with alt text</li>
+                                <li>Add a list with at least 3 elts</li>
+                                <li>Change bg color of header</li>
+                                <li>Add bootstrap button w text</li>
+                                <li>
+                                    Make button log Hello World! when clicked
+                                </li>
+                                <li>Have a 2-column layout on the page</li>
+                                <li>Put a red rectangle in each col</li>
+                            </ol>
+                        </Col>
+                        <Col>
+                            <Button onClick={() => console.log("Hello World!")}>
+                                Log Hello World
+                            </Button>
+                            <img
+                                src="../Pictures/fine.jpg"
+                                alt="This is fine."
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
