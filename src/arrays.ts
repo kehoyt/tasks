13e43bc51a7e2adc0a788443b4601c2b7f88ccd3
Katie <kehoyt@udel.edu>
2022-02-18 14:44:30 -0500

    tripleNumbers functional

--- src/arrays.ts
+++ src/arrays.ts
@@ -21,7 +21,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((num: number) => num * 3);
+    return tripled;
 }
 
 /**
