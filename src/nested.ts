0dd80b0c6ec8ac071302f49c8d692ec76275c0b1
Katie <kehoyt@udel.edu>
2022-02-27 13:59:15 -0500

    makeAnswers is functional

--- src/nested.ts
+++ src/nested.ts
@@ -131,7 +131,15 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    const answers = questions.map((question: Question): Answer => {
+        return {
+            questionId: question.id,
+            text: "",
+            submitted: false,
+            correct: false
+        };
+    });
+    return answers;
 }
 
 /***
