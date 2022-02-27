8d4f8ea0765b233f3d1dcc43147c61adc6f18f59
Katie <kehoyt@udel.edu>
2022-02-27 14:00:37 -0500

    publishAll is functional

--- src/nested.ts
+++ src/nested.ts
@@ -147,7 +147,10 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    const publishAll = questions.map((question: Question): Question => {
+        return { ...question, published: true };
+    });
+    return publishAll;
 }
 
 /***
