56addcb3d9e6ae22085b9efb93f8ad9775fa165d
Katie <kehoyt@udel.edu>
2022-02-27 13:39:04 -0500

    sumPoints is functional

--- src/nested.ts
+++ src/nested.ts
@@ -72,7 +72,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const sum = questions.reduce(
+        (currentSum: number, question: Question) =>
+            currentSum + question.points,
+        0
+    );
+    return sum;
 }
 
 /***
