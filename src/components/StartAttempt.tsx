dbc591d6bb37f62b72f66a3842d11d4743712a74
Katie <kehoyt@udel.edu>
2022-03-02 18:01:47 -0500

    StartAttempt is functional

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttemptsLeft] = useState<number>(4);
+    const [isInProgress, setInProgress] = useState<boolean>(false);
+
+    function newAttempt(): void {
+        flipInProgress();
+        setAttemptsLeft(attempts - 1);
+    }
+
+    function flipInProgress(): void {
+        setInProgress(!isInProgress);
+    }
+
+    function mulligan(): void {
+        setAttemptsLeft(attempts + 1);
+    }
+
+    return (
+        <div>
+            <Button
+                onClick={newAttempt}
+                disabled={isInProgress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={flipInProgress} disabled={!isInProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={mulligan} disabled={isInProgress}>
+                Mulligan
+            </Button>
+            <div>
+                Attempts: {attempts}, in progress:{" "}
+                {isInProgress ? "in progress" : "not in progress"}
+            </div>
+        </div>
+    );
 }
