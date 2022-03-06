5ee20e945d393b254c59cdaf87cdb182da17c904
Katie <kehoyt@udel.edu>
2022-03-06 14:28:28 -0500

    DoubleHalf is functional

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App(): JSX.Element {
                 Katie Hoyt - UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
