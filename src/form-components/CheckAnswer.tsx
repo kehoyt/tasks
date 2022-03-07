27a602275daefa2029c3a81d50ae320598e9d6ff
Katie <kehoyt@udel.edu>
2022-03-07 11:50:41 -0500

    CheckAnswer is functional

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, changeUserAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        changeUserAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group>
+                <Form.Control value={userAnswer} onChange={updateAnswer} />
+            </Form.Group>
+            <div>{expectedAnswer === userAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
